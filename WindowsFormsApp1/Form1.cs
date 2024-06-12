60b40b23cf9b3b632c2a63623213ea627366f5fc
Krzysztof Wala <krzysztof.wala@chropynska.pl>
2024-06-12 21:37:11 +0200

    Initial commit

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            multiSelectTreeView.Nodes.Add("Element 1");
+            multiSelectTreeView.Nodes.Add("Element 2");
+            multiSelectTreeView.Nodes.Add("Element 3");
+            multiSelectTreeView.Nodes.Add("Element 4");
+            multiSelectTreeView.Nodes.Add("Element 5");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.multiSelectTreeView.Refresh();
+        }
+    }
+}
---
+++ WindowsFormsApp1/MultiSelectTreeView.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class MultiSelectTreeView : TreeView
+    {
+        private List<TreeNode> selectedNodes;
+
+        public MultiSelectTreeView()
+        {
+            selectedNodes = new List<TreeNode>();
+        }
+
+        public List<TreeNode> SelectedNodes
+        {
+            get { return selectedNodes; }
+        }
+
+        protected override void OnNodeMouseClick(TreeNodeMouseClickEventArgs e)
+        {
+            base.OnNodeMouseClick(e);
+
+            if ((ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                if (selectedNodes.Contains(e.Node))
+                {
+                    selectedNodes.Remove(e.Node);
+                    e.Node.BackColor = SystemColors.Window;
+                    e.Node.ForeColor = SystemColors.ControlText; 
+                    e.Node.TreeView.Invalidate(e.Node.Bounds);
+                    this.Refresh();
+                }
+                else
+                {
+                    selectedNodes.Add(e.Node);
+                    e.Node.BackColor = SystemColors.Highlight;
+                    e.Node.ForeColor = SystemColors.HighlightText;
+                }
+            }
+            else
+            {
+                ClearSelectedNodes();
+                if (!selectedNodes.Contains(e.Node))
+                {
+                    selectedNodes.Add(e.Node);
+                    e.Node.BackColor = SystemColors.Highlight;
+                    e.Node.ForeColor = SystemColors.HighlightText;
+                }
+                else
+                {
+                    selectedNodes.Remove(e.Node);
+                    e.Node.BackColor = this.BackColor;
+                    e.Node.ForeColor = this.ForeColor;
+                }
+            }
+        }
+
+        public void ClearSelectedNodes()
+        {
+            foreach (TreeNode node in selectedNodes)
+            {
+                node.BackColor = this.BackColor;
+                node.ForeColor = this.ForeColor;
+            }
+            selectedNodes.Clear();
+        }
+
+        public List<TreeNode> GetSelectedNodes()
+        {
+            return selectedNodes;
+        }
+
+    }
+}
